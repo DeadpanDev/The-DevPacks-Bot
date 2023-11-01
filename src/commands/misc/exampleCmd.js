46edd83a64c4bb2bd2a423490e5985d339893851
DeadpanDev-linux <deadpangameryt@gmail.com>
2023-11-01 21:26:29 +0000

    First Commit

---
+++ src/commands/misc/exampleCmd.js
@@ -0,0 +1,56 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('test')
+        .setDescription('Test if everything is working correctly')
+        .setDMPermission(false)
+        .addSubcommandGroup((subcommandgroup) =>
+            subcommandgroup
+                .setName('user')
+                .setDescription('Configure a user.')
+                .addSubcommand((subcommand) =>
+                    subcommand
+                        .setName('role')
+                        .setDescription('Configure a users role.')
+                        .addUserOption((option) =>
+                            option
+                                .setName('user')
+                                .setDescription('The user to configure.')
+                        )
+                )
+                .addSubcommand((subcommand) =>
+                    subcommand
+                        .setName('nickname')
+                        .setDescription('Configure a users nickname.')
+                        .addUserOption((option) =>
+                            option
+                                .setName('nickname')
+                                .setDescription('The nickname the user should have.')
+                        )
+                        .addUserOption((option) =>
+                            option
+                                .setName('user')
+                                .setDescription('The user to configure.')
+                        )
+                )
+        )
+        .addSubcommand((subcommand) =>
+            subcommand.setName('message').setDescription('Configure a message.')
+        )
+        .toJSON(),
+    userPermissions: [PermissionFlagsBits.ManageMessages],
+    botPermissions: [PermissionFlagsBits.Connect],
+
+    run: (client, interaction) => {
+        const { EmbedBuilder, Colors } = require('discord.js');
+        const mConfig = require('../../messageConfig.json');
+        const createEmbed = (color, description) => new EmbedBuilder().setColor(color).setDescription(description);
+        const rEmbed = createEmbed(
+            mConfig.embedColorSuccess,
+            mConfig.testCommandSuccuss,
+        );
+        return interaction.reply({ embeds: [rEmbed], ephemeral: true });
+    },
+
+};
---
+++ src/events/ready/registerCommands.js
@@ -0,0 +1,68 @@
+require('colors');
+const { testServerId } = require('../../config.json');
+const commandComparing = require('../../utils/commandsComparing');
+const getApplicationCommands = require('../../utils/getApplicationCommands');
+const getLocalCommands = require('../../utils/getLocalCommands');
+
+module.exports = async (client) => {
+    try {
+        const [localCommands, applicationCommands] = await Promise.all([
+            getLocalCommands(),
+            getApplicationCommands(client, testServerId),
+        ]);
+
+        for (const localCommand of localCommands) {
+            const { data, deleted } = localCommand;
+            const {
+                name: commandName,
+                description: commandDescription,
+                options: commandOptions,
+            } = data;
+
+            const existingCommand = await applicationCommands.cache.find(
+                (cmd) => cmd.name === commandName
+            );
+
+            if (deleted) {
+                if (existingCommand) {
+                    await existingCommand.delete(existingCommand.id);
+                    console.log(
+                        `[COMMAND REGISTRY] Application command ${commandName} has been deleted.`
+                            .red
+                    );
+                } else {
+                    console.log(
+                        `[COMMAND REGISTRY] Application command ${commandName} has been skipped, since property 'deleted' is set to true.`
+                            .gray
+                    );
+                }
+            } else if (existingCommand) {
+                if (commandComparing(existingCommand, localCommand)) {
+                    await applicationCommands.edit(existingCommand.id, {
+                        name: commandName,
+                        description: commandDescription,
+                        options: commandOptions,
+                    });
+                    console.log(
+                        `[COMMAND REGISTRY] Application command ${commandName} has been edited.`
+                            .yellow
+                    );
+                }
+            } else {
+                await applicationCommands.create({
+                    name: commandName,
+                    description: commandDescription,
+                    options: commandOptions,
+                });
+                console.log(
+                    `[COMMAND REGISTRY] Application command ${commandName} has been registered.`
+                        .green
+                );
+            }
+        }
+    } catch (error) {
+        console.log(
+            `[ERROR] An error occurred inside the command registry:\n${error}`.red
+        );
+    }
+};
